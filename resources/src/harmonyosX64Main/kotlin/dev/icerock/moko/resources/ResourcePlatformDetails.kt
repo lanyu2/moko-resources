2451748006eb55b00a37fe04b6b3135c09b8a4fb
lanyu2 <1141521742@qq.com>
2026-01-09 16:17:36 +0800

    添加有用的属性

--- resources/src/harmonyosX64Main/kotlin/dev/icerock/moko/resources/ResourcePlatformDetails.kt
+++ resources/src/harmonyosX64Main/kotlin/dev/icerock/moko/resources/ResourcePlatformDetails.kt
@@ -4,4 +4,24 @@
 
 package dev.icerock.moko.resources
 
-actual class ResourcePlatformDetails
+/**
+ * HarmonyOS Native 平台资源详情
+ */
+actual class ResourcePlatformDetails(
+    val resourcesBasePath: String = "/res/raw"
+) {
+    /**
+     * 获取资源完整路径
+     */
+    fun getFullPath(relativePath: String): String {
+        return "$resourcesBasePath/$relativePath"
+    }
+
+    override fun equals(other: Any?): Boolean {
+        if (this === other) return true
+        if (other !is ResourcePlatformDetails) return false
+        return resourcesBasePath == other.resourcesBasePath
+    }
+
+    override fun hashCode(): Int = resourcesBasePath.hashCode()
+}
