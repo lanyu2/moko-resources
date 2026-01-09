5de86ee27784063596c40c4d39fbf04b11649875
lanyu2 <1141521742@qq.com>
2026-01-09 16:14:57 +0800

    修改方法

--- resources/src/harmonyosX64Main/kotlin/dev/icerock/moko/resources/desc/DescExtensions.kt
+++ resources/src/harmonyosX64Main/kotlin/dev/icerock/moko/resources/desc/DescExtensions.kt
@@ -6,11 +6,42 @@ package dev.icerock.moko.resources.desc
 
 import dev.icerock.moko.resources.PluralsResource
 import dev.icerock.moko.resources.StringResource
-import kotlin.experimental.ExperimentalNativeApi
 
-@OptIn(ExperimentalNativeApi::class)
-@CName("Int_strResDesc")
-fun Int.strResDesc() = StringResource(this).desc()
-@OptIn(ExperimentalNativeApi::class)
-@CName("Int_plrResDesc")
-fun Int.plrResDesc(number: Int) = PluralsResource(this).desc(number)
+/**
+ * HarmonyOS Native 平台的资源描述符扩展
+ *
+ * 注意：此平台不支持基于 Int ID 的资源访问
+ * 请使用以下方式：
+ *   - MR.strings.xxx.desc()
+ *   - MR.plurals.xxx.desc(number)
+ */
+
+// ===== StringResource 扩展 =====
+
+fun StringResource.desc(): ResourceStringDesc = ResourceStringDesc(this)
+
+fun StringResource.desc(vararg args: Any): ResourceFormattedStringDesc =
+    ResourceFormattedStringDesc(this, args.toList())
+
+// ===== PluralsResource 扩展 =====
+
+fun PluralsResource.desc(number: Int): PluralStringDesc = PluralStringDesc(this, number)
+
+fun PluralsResource.desc(number: Int, vararg args: Any): PluralFormattedStringDesc =
+    PluralFormattedStringDesc(this, number, args.toList())
+
+// ===== 已废弃的 Int 扩展（编译时错误）=====
+
+@Deprecated(
+    message = "HarmonyOS Native 不支持基于 Int 的资源访问，请使用 MR.strings.xxx.desc()",
+    level = DeprecationLevel.ERROR
+)
+fun Int.strResDesc(): Nothing =
+    throw UnsupportedOperationException("Use MR.strings.xxx.desc() instead")
+
+@Deprecated(
+    message = "HarmonyOS Native 不支持基于 Int 的资源访问，请使用 MR.plurals.xxx.desc(number)",
+    level = DeprecationLevel.ERROR
+)
+fun Int.plrResDesc(number: Int): Nothing =
+    throw UnsupportedOperationException("Use MR.plurals.xxx.desc(number) instead")
